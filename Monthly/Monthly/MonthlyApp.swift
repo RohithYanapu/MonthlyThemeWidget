ca0498782a8ca8fdbcaac777b8942dd477b80f5e
RohithYanapu <110309836+RohithYanapu@users.noreply.github.com>
2025-06-08 18:09:51 -0500

    Initial Commit

---
+++ Monthly/Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Rohith Yanapu on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
